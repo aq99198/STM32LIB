69bf6c753832bd7434d6579912ba4d1631e2173d
jabezGit <jinjiabinshihaoren@gmail.com>
2016-09-22 22:30:11 +0800

    USART

---
+++ USART/src/init.c
@@ -0,0 +1,17 @@
+#include "board.h"
+#include "init.h"
+
+void SysInit(void){
+	
+	// Turn on clocks for stuff we use
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 | RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM4, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_TIM1 | RCC_APB2Periph_ADC1 | RCC_APB2Periph_USART1, ENABLE);
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+	
+	 // system tick
+		cycleCounterInit();
+		SysTick_Config (SystemCoreClock / 1000);
+}
+
+
+
