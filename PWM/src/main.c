8fbba6f17330edd5d5b8dad3c122c16fe213472a
jabezGit <jinjiabinshihaoren@gmail.com>
2016-09-24 14:11:43 +0800

    PWM

---
+++ PWM/src/main.c
@@ -0,0 +1,38 @@
+#include "board.h"
+#include "init.h"
+
+core_t core;
+drv_pwm_config_t pwm_params;
+
+int fputc(int c, FILE *f)
+{
+    // let DMA catch up a bit when using set or dump, we're too fast.
+    //while (!serialTotalBytesWaiting(core.mainport));
+    serialWrite(core.mainport, c);
+    return c;
+}
+
+int main(void)
+{
+		SysInit();
+	
+		core.mainport = uartOpen(USART1, NULL, 9600, MODE_RXTX);
+		core.flexport = uartOpen(USART3, NULL, 9600, MODE_RXTX);
+		pwm_params.airplane=true;
+		pwm_params.motorPwmRate=4000;
+		pwm_params.servoPwmRate=500;
+		pwm_params.idlePulse = PULSE_1MS;
+		pwm_params.useServos = true;
+		pwm_params.servoCenterPulse = 1500;
+		pwmInit(&pwm_params);
+		pwmWriteServo(0,1500);
+		pwmWriteServo(1,1500);
+		pwmWriteServo(2,1500);
+		
+	while(1){
+		
+		printf("jin \r\n");
+		delay(20);
+	}
+}
+			
